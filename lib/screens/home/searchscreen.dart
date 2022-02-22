dd9dffbd4215fb4c6314610c9a8a0918ca32bcec
Anugrah Surya Putra <putra.183040095@mail.unpas.ac.id>
2022-02-23 02:19:00 +0700

    add searchscreen

--- lib/screens/home/searchscreen.dart
+++ lib/screens/home/searchscreen.dart
@@ -1,14 +1,98 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class Searchscreen extends StatelessWidget {
-  const Searchscreen({Key? key}) : super(key: key);
+import '../../providers/book_provider.dart';
+import '../../themes.dart';
+
+class Searchscreen extends StatefulWidget {
+  const Searchscreen({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Searchscreen> createState() => _SearchscreenState();
+}
+
+class _SearchscreenState extends State<Searchscreen> {
+  // final TextEditingController _textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    final data = Provider.of<BookProvider>(context);
+    var book = data.books;
+
     return Scaffold(
-      body: Center(
-        child: Text('Search Screen'),
-      ),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 23),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.filter_list_rounded,
+                  color: Colors.black,
+                  size: 28,
+                ),
+              ),
+            ),
+          ],
+          title: Container(
+            padding: const EdgeInsets.only(left: 10),
+            width: width,
+            height: 35,
+            decoration: BoxDecoration(
+              color: const Color(0xffE1E1E1),
+              borderRadius: BorderRadius.circular(9),
+            ),
+            child: Row(
+              children: [
+                const Icon(
+                  Icons.search_rounded,
+                  color: Color(0xff8A8A8A),
+                  size: 16,
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: TextFormField(
+                    // controller: _textEditingController,
+                    style: primaryText.copyWith(
+                      fontSize: 13,
+                    ),
+                    decoration: InputDecoration.collapsed(
+                      hintText: 'Search',
+                      hintStyle: secondaryText.copyWith(fontSize: 13),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        body: noResult());
+  }
+
+  Column noResult() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Container(
+            margin: const EdgeInsets.symmetric(
+              horizontal: 30,
+            ),
+            child: Text(
+              'No Results!',
+              style: primaryText.copyWith(fontSize: 18),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ lib/widgets/search_card.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../themes.dart';
+
+class SearchCard extends StatelessWidget {
+  const SearchCard({
+    Key? key,
+    required this.id,
+    required this.title,
+    required this.image,
+    required this.author,
+    required this.price,
+  }) : super(key: key);
+
+  final String id;
+  final String title;
+  final String image;
+  final String author;
+  final String price;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.pushNamed(context, '/detail', arguments: id);
+      },
+      child: Container(
+        margin: const EdgeInsets.only(left: 30, bottom: 20),
+        child: Row(
+          children: [
+            Image.asset(
+              image,
+              height: 89,
+            ),
+            const SizedBox(
+              width: 10,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.6,
+                  child: Text(
+                    title,
+                    style: primaryText.copyWith(
+                      fontSize: 14,
+                      fontWeight: semiBold,
+                    ),
+                  ),
+                ),
+                Text(
+                  author,
+                  style: secondaryText.copyWith(
+                    fontSize: 12,
+                    fontWeight: medium,
+                  ),
+                ),
+                const SizedBox(height: 33),
+                Text(
+                  '\$$price',
+                  style: primaryText.copyWith(
+                    fontSize: 14,
+                    fontWeight: semiBold,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
